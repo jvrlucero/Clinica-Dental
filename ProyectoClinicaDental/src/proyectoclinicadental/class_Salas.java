3394389075ac3281bfec7aaf6d768624d90b43bd
Javier Lucero <jvrlucero724@gmail.com>
2018-07-01 00:37:59 -0600

    :v

---
+++ ProyectoClinicaDental/src/proyectoclinicadental/class_Salas.java
@@ -0,0 +1,19 @@
+
+package proyectoclinicadental;
+
+import java.sql.ResultSet;
+
+public class class_Salas {
+    
+    Conexion cn = new Conexion();
+    public void insertar(String Nombre) {
+        cn.UID("insert into salas(Nombre_Sala) value('"+Nombre+"')");
+    }
+    public void modificar(String ID, String Nombre){
+        cn.UID("Update salas set Nombre_Sala='"+Nombre+"' where id_Sala='"+ID+"'");
+    }
+    public ResultSet LlenarListadeSalas() {
+        return (cn.getValores("select Nombre_Sala from salas"));
+    }
+    
+}
