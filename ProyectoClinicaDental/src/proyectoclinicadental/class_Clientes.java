48e9c28951c47a80ce14a32448d2deaf08e45e9a
Javier Lucero <jvrlucero724@gmail.com>
2018-06-30 22:06:33 -0600

    :v

---
+++ ProyectoClinicaDental/src/proyectoclinicadental/class_Clientes.java
@@ -0,0 +1,11 @@
+
+package proyectoclinicadental;
+
+import java.sql.ResultSet;
+public class class_Clientes {
+    //Conexion cn = new Conexion();
+    
+   /* public void insertar(String Nombre,String Apellido,String Tel,String Direccion,String Edad) {
+        cn.UID("insert into clientes(Nombres_Cli,Apellidos_Cli,Tel_Cli,Direccion_Cli,Edad_Cli) values('"+Nombre+"','"+Apellido+"','"+Tel+"','"+Direccion+"','"+Edad+"')");
+    }*/
+}
