163565dcbad8351b12811f20ddd79c002dd607dd
Javier Lucero <jvrlucero724@gmail.com>
2018-06-30 23:46:11 -0600

    :v

--- ProyectoClinicaDental/src/proyectoclinicadental/class_Clientes.java
+++ ProyectoClinicaDental/src/proyectoclinicadental/class_Clientes.java
@@ -3,9 +3,35 @@ package proyectoclinicadental;
 
 import java.sql.ResultSet;
 public class class_Clientes {
-    //Conexion cn = new Conexion();
+    Conexion cn = new Conexion();
     
-   /* public void insertar(String Nombre,String Apellido,String Tel,String Direccion,String Edad) {
+    
+    public void insertar(String Nombre,String Apellido,String Tel,String Direccion,String Edad) {
         cn.UID("insert into clientes(Nombres_Cli,Apellidos_Cli,Tel_Cli,Direccion_Cli,Edad_Cli) values('"+Nombre+"','"+Apellido+"','"+Tel+"','"+Direccion+"','"+Edad+"')");
-    }*/
+    }
+    
+    public ResultSet contarRegistros() {
+        return (cn.getValores("SELECT COUNT(id_Cli) FROM clientes"));
+    }
+    
+    public ResultSet mayorRegistro() {
+        return (cn.getValores("SELECT MAX(id_Cli) FROM clientes"));
+    }
+    
+    public ResultSet llenarTabla() {
+        return (cn.getValores("SELECT * FROM clientes"));
+    }
+    
+    public void modificar(String ID,String Nombre,String Apellido,String Tel,String Direccion,String Edad) {
+        cn.UID("update clientes set Nombres_Cli='"+Nombre+"',Apellidos_Cli='"+Apellido+"',Tel_Cli='"+Tel+"',Direccion_Cli='"+Direccion+"',Edad_Cli='"+Edad+"' where id_Cli='"+ID+"'");
+    }
+    
+    public ResultSet ConsultaporPorCodigo(String ID) {
+        return (cn.getValores("SELECT * FROM clientes where id_Cli='"+ID+"'"));
+    }
+    
+    public void eliminar(String ID) {
+        cn.UID("DELETE FROM clientes where id_Cli='"+ID+"'");
+    }
+
 }
