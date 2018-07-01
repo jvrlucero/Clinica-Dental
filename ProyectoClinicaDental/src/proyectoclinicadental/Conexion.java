a277d326b118cb2d7946e41361cb2c013f1de3cc
Javier Lucero <jvrlucero724@gmail.com>
2018-06-30 22:11:00 -0600

    :v

---
+++ ProyectoClinicaDental/src/proyectoclinicadental/Conexion.java
@@ -0,0 +1,13 @@
+
+package proyectoclinicadental;
+
+import java.sql.Connection; //Puntero a la base de datos, proporciona el contexto para ResultSet y Statement
+import java.sql.DriverManager; //Lleva el control de los gestores JDBC disponibles
+import java.sql.ResultSet; //Contiene los datos resultado de una sentencia SQL
+import java.sql.SQLException;
+import java.sql.Statement; //Ejecución de una sentencia SQL, devuelve un ResultSet
+import javax.swing.JOptionPane;
+
+public class Conexion {
+    
+}
