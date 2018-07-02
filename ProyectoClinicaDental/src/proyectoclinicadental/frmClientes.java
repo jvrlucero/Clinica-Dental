f1d633564d9cc4e69eb1bb44bd852f54a73cf265
Javier Lucero <jvrlucero724@gmail.com>
2018-07-01 20:10:48 -0600

    :v

--- ProyectoClinicaDental/src/proyectoclinicadental/frmClientes.java
+++ ProyectoClinicaDental/src/proyectoclinicadental/frmClientes.java
@@ -2,7 +2,9 @@
 package proyectoclinicadental;
 
 import java.awt.Color;
+import java.awt.Component;
 import javax.swing.JOptionPane;
+
 
 public class frmClientes extends javax.swing.JFrame {
 
@@ -20,6 +22,7 @@ public class frmClientes extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
+        jLabel1 = new javax.swing.JLabel();
         lblTemaClientes = new javax.swing.JLabel();
         lblNombre = new javax.swing.JLabel();
         lblApellidoCliente = new javax.swing.JLabel();
@@ -34,7 +37,7 @@ public class frmClientes extends javax.swing.JFrame {
         btnAgregarCliente = new javax.swing.JToggleButton();
         btnLimpiarCliente = new javax.swing.JToggleButton();
         btnRegresarCliente = new javax.swing.JToggleButton();
-        jLabel1 = new javax.swing.JLabel();
+        jButton1 = new javax.swing.JButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         getContentPane().setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
@@ -64,26 +67,72 @@ public class frmClientes extends javax.swing.JFrame {
         lblDireccionCliente.setToolTipText("");
         getContentPane().add(lblDireccionCliente, new org.netbeans.lib.awtextra.AbsoluteConstraints(60, 340, 70, 30));
 
-        txtNombreCliente.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                txtNombreClienteActionPerformed(evt);
+        txtNombreCliente.addKeyListener(new java.awt.event.KeyAdapter() {
+            public void keyTyped(java.awt.event.KeyEvent evt) {
+                txtNombreClienteKeyTyped(evt);
             }
         });
         getContentPane().add(txtNombreCliente, new org.netbeans.lib.awtextra.AbsoluteConstraints(160, 100, 270, 30));
+
+        txtApellidoCliente.addKeyListener(new java.awt.event.KeyAdapter() {
+            public void keyTyped(java.awt.event.KeyEvent evt) {
+                txtApellidoClienteKeyTyped(evt);
+            }
+        });
         getContentPane().add(txtApellidoCliente, new org.netbeans.lib.awtextra.AbsoluteConstraints(160, 160, 270, -1));
+
+        txtTelefonoCliente.addKeyListener(new java.awt.event.KeyAdapter() {
+            public void keyTyped(java.awt.event.KeyEvent evt) {
+                txtTelefonoClienteKeyTyped(evt);
+            }
+        });
         getContentPane().add(txtTelefonoCliente, new org.netbeans.lib.awtextra.AbsoluteConstraints(160, 220, 120, -1));
+
+        txtEdadCliente.addKeyListener(new java.awt.event.KeyAdapter() {
+            public void keyTyped(java.awt.event.KeyEvent evt) {
+                txtEdadClienteKeyTyped(evt);
+            }
+        });
         getContentPane().add(txtEdadCliente, new org.netbeans.lib.awtextra.AbsoluteConstraints(160, 280, 60, -1));
+
+        txtDireccionCliente.addKeyListener(new java.awt.event.KeyAdapter() {
+            public void keyTyped(java.awt.event.KeyEvent evt) {
+                txtDireccionClienteKeyTyped(evt);
+            }
+        });
         getContentPane().add(txtDireccionCliente, new org.netbeans.lib.awtextra.AbsoluteConstraints(160, 340, 270, -1));
 
         btnAgregarCliente.setText("AGREGAR");
+        btnAgregarCliente.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnAgregarClienteActionPerformed(evt);
+            }
+        });
         getContentPane().add(btnAgregarCliente, new org.netbeans.lib.awtextra.AbsoluteConstraints(80, 420, 120, -1));
 
         btnLimpiarCliente.setText("LIMPIAR");
+        btnLimpiarCliente.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnLimpiarClienteActionPerformed(evt);
+            }
+        });
         getContentPane().add(btnLimpiarCliente, new org.netbeans.lib.awtextra.AbsoluteConstraints(210, 420, 90, -1));
 
         btnRegresarCliente.setText("REGRESAR");
+        btnRegresarCliente.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnRegresarClienteActionPerformed(evt);
+            }
+        });
         getContentPane().add(btnRegresarCliente, new org.netbeans.lib.awtextra.AbsoluteConstraints(330, 420, 120, -1));
-        getContentPane().add(jLabel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(10, -30, 1240, 840));
+
+        jButton1.setText("AÑADIR ELEMENTO AL HISTORIAL MÉDICO");
+        jButton1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton1ActionPerformed(evt);
+            }
+        });
+        getContentPane().add(jButton1, new org.netbeans.lib.awtextra.AbsoluteConstraints(80, 460, 370, -1));
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
@@ -114,65 +163,62 @@ public class frmClientes extends javax.swing.JFrame {
             RegistrarCliente();
         mens="";
     }
-    private void btnAgregarClienteActionPerformed(java.awt.event.ActionEvent evt) {                                                  
+    private void btnAgregarClienteActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnAgregarClienteActionPerformed
        comprobarcampos();
-    }                                                 
+    }//GEN-LAST:event_btnAgregarClienteActionPerformed
 
-    private void txtNombreClienteKeyTyped(java.awt.event.KeyEvent evt) {                                          
+    private void txtNombreClienteKeyTyped(java.awt.event.KeyEvent evt) {//GEN-FIRST:event_txtNombreClienteKeyTyped
         if(txtNombreCliente.getText().length()==50)
             evt.consume();
         if(!Character.isLetter(evt.getKeyChar()))
             evt.consume();
-    }                                         
+    }//GEN-LAST:event_txtNombreClienteKeyTyped
 
-    private void txtApellidoClienteKeyTyped(java.awt.event.KeyEvent evt) {                                            
+    private void txtApellidoClienteKeyTyped(java.awt.event.KeyEvent evt) {//GEN-FIRST:event_txtApellidoClienteKeyTyped
         if(txtApellidoCliente.getText().length()==50)
             evt.consume();
         if(!Character.isLetter(evt.getKeyChar()))
             evt.consume();
-    }                                           
+    }//GEN-LAST:event_txtApellidoClienteKeyTyped
 
-    private void txtTelefonoClienteKeyTyped(java.awt.event.KeyEvent evt) {                                            
+    private void txtTelefonoClienteKeyTyped(java.awt.event.KeyEvent evt) {//GEN-FIRST:event_txtTelefonoClienteKeyTyped
         if(txtTelefonoCliente.getText().length()==8)
             evt.consume();
         if(!Character.isDigit(evt.getKeyChar()))
             evt.consume();
-    }                                           
+    }//GEN-LAST:event_txtTelefonoClienteKeyTyped
 
-    private void txtDireccionClienteKeyTyped(java.awt.event.KeyEvent evt) {                                             
+    private void txtDireccionClienteKeyTyped(java.awt.event.KeyEvent evt) {//GEN-FIRST:event_txtDireccionClienteKeyTyped
         if(txtDireccionCliente.getText().length()==100)
             evt.consume();
-    }                                            
+    }//GEN-LAST:event_txtDireccionClienteKeyTyped
 
-    private void txtEdadClienteKeyTyped(java.awt.event.KeyEvent evt) {                                        
+    private void txtEdadClienteKeyTyped(java.awt.event.KeyEvent evt) {//GEN-FIRST:event_txtEdadClienteKeyTyped
         if(txtEdadCliente.getText().length()==2)
             evt.consume();
-    }                                       
+        if(!Character.isDigit(evt.getKeyChar()))
+            evt.consume();
+    }//GEN-LAST:event_txtEdadClienteKeyTyped
 
-    private void btnLimpiarClienteActionPerformed(java.awt.event.ActionEvent evt) {                                                  
+    private void btnLimpiarClienteActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnLimpiarClienteActionPerformed
         txtNombreCliente.setText("");
         txtApellidoCliente.setText("");
         txtTelefonoCliente.setText("");
         txtEdadCliente.setText("");
         txtDireccionCliente.setText("");
-    }                                                 
+    }//GEN-LAST:event_btnLimpiarClienteActionPerformed
 
-    private void btnRegresarClienteActionPerformed(java.awt.event.ActionEvent evt) {                                                   
+    private void btnRegresarClienteActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnRegresarClienteActionPerformed
         frmMenuPrincipal menu=new frmMenuPrincipal();
         this.dispose();
         menu.setVisible(true);
-    }                                                  
+    }//GEN-LAST:event_btnRegresarClienteActionPerformed
 
-    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {                                         
+    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
         frmHistorialCliente HC=new frmHistorialCliente();
         this.dispose();
         HC.setVisible(true);
-    }
-    
-    private void txtNombreClienteActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_txtNombreClienteActionPerformed
-        // TODO add your handling code here:
-        
-    }//GEN-LAST:event_txtNombreClienteActionPerformed
+    }//GEN-LAST:event_jButton1ActionPerformed
 
     
     public static void main(String args[]) {
@@ -211,6 +257,7 @@ public class frmClientes extends javax.swing.JFrame {
     private javax.swing.JToggleButton btnAgregarCliente;
     private javax.swing.JToggleButton btnLimpiarCliente;
     private javax.swing.JToggleButton btnRegresarCliente;
+    private javax.swing.JButton jButton1;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel lblApellidoCliente;
     private javax.swing.JLabel lblDireccionCliente;
@@ -224,4 +271,6 @@ public class frmClientes extends javax.swing.JFrame {
     private javax.swing.JTextField txtNombreCliente;
     private javax.swing.JTextField txtTelefonoCliente;
     // End of variables declaration//GEN-END:variables
+
+    
 }
